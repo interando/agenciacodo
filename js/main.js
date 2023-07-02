45dfb1324d7289fd3c43ba09eff25736332c3879
German Bruel <interando@gmail.com>
2023-07-02 17:18:37 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,8 +35,7 @@ document.getElementById('footer').innerHTML = `
   </p>
 
   <ul class="nav col-md-4 justify-content-end">
-    <li class="nav-item text-primary">Germán Brüel</li>
-    <li class="nav-item text-primary">Marta Levalle</li>
+    <li class="nav-item text-primary">Grupo 16: Germán Brüel | Marta Levalle</li>
   
   </ul>
 </div>
